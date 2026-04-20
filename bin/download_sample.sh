ec4e9395e39a0c48b2cfa451e3f903d13d9fef47
ctuni <c.tuni.dominguez@gmail.com>
2026-04-20 16:26:13 +0200

    api rate limit error is back

--- bin/download_sample.sh
+++ bin/download_sample.sh
@@ -55,7 +55,7 @@ download_ena() {
     [[ "${DATASET,,}" =~ ^block ]] && provider_flag='--provider sra --only-provider'
     [[ "${DATASET,,}" =~ ^ibarra ]] && provider_flag='--provider sra --only-provider'
     [[ "${DATASET,,}" =~ ^ngo ]] && provider_flag='--provider sra --only-provider'
-    fastq-dl --accession "${accession}" --outdir . --provider sra --only-provider
+    fastq-dl --accession "${accession}" --outdir . ${provider_flag} 
 
     # SE: fastq-dl produces <accession>.fastq.gz (no _1 suffix); rename for consistency
     if [[ ! -f "${accession}_1.fastq.gz" && -f "${accession}.fastq.gz" ]]; then
