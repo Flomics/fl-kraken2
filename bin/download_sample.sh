decef8ac2a28f5d8fbe89421dd947aa12eb98031
ctuni <c.tuni.dominguez@gmail.com>
2026-04-20 12:20:21 +0200

    trying to solve the bottleneck

--- bin/download_sample.sh
+++ bin/download_sample.sh
@@ -55,7 +55,7 @@ download_ena() {
     [[ "${DATASET,,}" =~ ^block ]] && provider_flag='--provider sra --only-provider'
     [[ "${DATASET,,}" =~ ^ibarra ]] && provider_flag='--provider sra --only-provider'
     [[ "${DATASET,,}" =~ ^ngo ]] && provider_flag='--provider sra --only-provider'
-    fastq-dl --accession "${accession}" --outdir . ${provider_flag}
+    fastq-dl --accession "${accession}" --outdir . ${provider_flag} --protocol https
 
     # SE: fastq-dl produces <accession>.fastq.gz (no _1 suffix); rename for consistency
     if [[ ! -f "${accession}_1.fastq.gz" && -f "${accession}.fastq.gz" ]]; then
